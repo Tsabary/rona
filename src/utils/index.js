bd1cc0a3fb606a2063f5e3f1de4b0454653fe5fd
moshe vilner <shootermv@gmail.com>
2020-03-26 14:26:05 +0200

    refactoring display of distance

--- src/utils/index.js
+++ src/utils/index.js
@@ -74,7 +74,7 @@ export const addLineBreaks = string =>
   export function calcGeoDistance(post ,cb) {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(({coords:{latitude: lat, longitude:lng}}) => {
-        cb(calcDistance(lat, lng, post._geoloc.lat, post._geoloc.lng, 'K').toFixed(2));
+        cb(calcDistance(lat, lng, post._geoloc.lat, post._geoloc.lng, 'K').toFixed(1));
       })
     } else {
       cb(0);
