bfdedef9a7c4c33b960f177fe3d8413befea8c2d
Moshe <shootermv@gmail.com>
2020-03-27 15:57:52 +0300

    created addressOtions component at the header (#14)

--- src/components/header/authOptions/index.js
+++ src/components/header/authOptions/index.js
@@ -1,16 +1,15 @@
 import "./styles.scss";
 import React from "react";
 
-import TextButton from "../../formComponents/textButton";
 import BoxedButton from "../../formComponents/boxedButton";
 
 const AuthOptions = () => {
   return (
     <div className="auth-options">
-    <a href="#sign-up">
-      <BoxedButton text="התחברות" />
-    </a>
-  </div>
+      <a href="#sign-up">
+        <BoxedButton text="התחברות" />
+      </a>
+    </div>
   );
 };
 
