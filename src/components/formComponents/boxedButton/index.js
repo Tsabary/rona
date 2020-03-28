2df69d7793faeb6fe2719304d0ebb3b4657e3e59
moshe vilner <shootermv@gmail.com>
2020-03-28 22:47:35 +0300

    restored look of submit-new-request button

--- src/components/formComponents/boxedButton/index.js
+++ src/components/formComponents/boxedButton/index.js
@@ -1,7 +1,14 @@
 import "./styles.scss";
 import React from "react";
 
-const BoxedButton = ({ text, action }) => {
+const BoxedButton = ({ text, action, children }) => {
+  if (children) {
+    return (
+      <button type="submit" className="boxed-button">
+        {children}
+      </button>
+    )
+  }
   return (
     <div className="boxed-button" onClick={action}>
       {text}
