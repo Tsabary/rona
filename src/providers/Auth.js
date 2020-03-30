25a1d4da87aab6f3442b80a033f513dc9692f2f2
moshe vilner <shootermv@gmail.com>
2020-03-30 18:25:28 +0300

    extra scripts removed

--- src/providers/Auth.js
+++ src/providers/Auth.js
@@ -10,7 +10,7 @@ export const AuthContext = React.createContext();
 
 ReactGA.initialize(process.env.REACT_APP_GA);
 
-
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
